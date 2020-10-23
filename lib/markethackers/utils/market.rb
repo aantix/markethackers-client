01c641a7b75be041a098d279e82e0b2cccbfa532
Jim Jones <jim.jones1@gmail.com>
2020-10-22 22:48:03 -0500

    Fixed bug with incorrect usage of time zone and market open/closed

--- lib/markethackers/utils/market.rb
+++ lib/markethackers/utils/market.rb
@@ -3,13 +3,13 @@ module Markethackers
     class Market
       ZONE  = Time.find_zone("Eastern Time (US & Canada)")
 
-      OPEN  = Time.ZONE.parse("9:30am")
-      CLOSE = Time.ZONE.parse("4:00pm")
+      OPEN  = ZONE.parse("9:30am")
+      CLOSE = ZONE.parse("4:00pm")
 
       def self.open?
         now = ZONE.now
 
-        now > OPEN && now < CLOSE
+        now >= OPEN && now <= CLOSE
       end
 
       def self.closed?
